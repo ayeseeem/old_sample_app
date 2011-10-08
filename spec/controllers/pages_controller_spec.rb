3b4f978394c020b9265b86a9665a7ae395de842f
ayeseeem <ayeseeem@gmail.com>
2011-10-08 17:19:35 +0100

    refactor tests to use common base title

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'about'" do
     it "should be successful" do
@@ -11,8 +15,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", 
-                        :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
     end
   end
 
@@ -24,9 +28,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", 
-                        :content => 
-                          "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contact")
     end
   end
 
@@ -38,8 +41,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", 
-                        :content => "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_selector("title",
+                                    :content => @base_title + " | Help")
     end
   end
 
@@ -51,8 +54,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", 
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
     end
   end
 end
